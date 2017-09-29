29e6c00af89a40b95aba19547a8c4607d13cd154
Snap IT Solutions LLC <ram@snapit.solutions>
2017-09-28 22:46:32 -0500

    Git demo check in

---
+++ src/basics1/EvenOrOdd.java
@@ -0,0 +1,25 @@
+package basics1;
+
+public class EvenOrOdd {
+
+	public static void main(String[] args) {
+
+		// call/invoke method evenOrOdd which takes an argument(input) an integer
+		evenOrOdd(5);
+
+	}
+
+	private static void evenOrOdd(int number) {
+
+		//if the number modulus 2(i.e remainder is 0), the number is odd
+		if (number % 2 == 0) {
+			System.out.println("number is even");
+		}
+
+		//else number is 0
+		else
+			System.out.println("number is odd");
+
+	}
+
+}
---
+++ src/basics1/GradeStudents.java
@@ -0,0 +1,28 @@
+package basics1;
+
+public class GradeStudents {
+
+	public static void main(String[] args) {
+		checkStudentGrade(65);
+	}
+
+	private static void checkStudentGrade(int score) {
+
+		if (score < 50) {
+			System.out.println("fail");
+		} else if (score >= 50 && score < 60) {
+			System.out.println("D grade");
+		} else if (score >= 60 && score < 70) {
+			System.out.println("C grade");
+		} else if (score >= 70 && score < 80) {
+			System.out.println("B grade");
+		} else if (score >= 80 && score < 90) {
+			System.out.println("A grade");
+		} else if (score >= 90 && score < 100) {
+			System.out.println("A+ grade");
+		} else {
+			System.out.println("Invalid!");
+		}
+	}
+
+}
